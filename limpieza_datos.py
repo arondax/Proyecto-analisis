17dc06e518e74f7a7794c4acfb9d7da1a9fface1
arondax <alexronda30@gmail.com>
2026-04-08 01:55:40 +0200

    Limpieza de columnas no necesarias(id, jugadord, modo, composicion y rol), obtencion valor main,

--- limpieza_datos.py
+++ limpieza_datos.py
@@ -21,8 +21,29 @@ def limpieza_jugador(nombre_jugador):
     print("-------------------------")
     print(df.info())
     
+    #Borramos las entradas donde el modo no sea competitivo
+    print(df['modo'].unique())
+    df = borrar_no_competitivo(df)
+    print(df['modo'].unique())
     
+    #Obtenemos el main
+    df= obtener_main(df)
     
+    print("-------------------------")
+    print(df.info())
+    
+    #Con los datos limpios borramos las columnas id_partida, jugador, modo
+    columnas_a_eliminar = ['id_partida', 'jugador', 'modo', 'composición', 'rol']
+
+    for col in columnas_a_eliminar:
+        if col in df.columns:
+            df = df.drop(columns=[col])
+            print(f"Columna '{col}' borrada.")
+        else:
+            print(f"La columna '{col}' no existía o ya fue borrada.")   
+    
+    print("-------------------------")
+    print(df.info())
     return None
 
 """
@@ -35,8 +56,27 @@ def quitar_duplicados(df):
     
     return df_limpio
 
+def obtener_main(df):
+    #suponemos que el orden es cornológico
+    df['bloque_10'] = np.arange(len(df)) // 10
+    
+    #Personaje mas usado por bloque:
+    main_bloque = df.groupby('bloque_10')['personaje'].apply(
+        lambda x: x.value_counts().idxmax()
+    )
+    #Resultado y mapeo
+    df['personaje_main_del_bloque'] = df['bloque_10'].map(main_bloque)
+    
+    #Añadimos a la columna binaria
+    df['es_main'] = (df['personaje'] == df['personaje_main_del_bloque']).astype(int)
+    
+    df.drop(columns=['bloque_10', 'personaje_main_del_bloque', 'personaje'], inplace=True)
+    
+    return df
 
-
+def borrar_no_competitivo(df):
+    df_limpio= df[df['modo'].str.contains('Competitive')]
+    return df_limpio
 
 
 prueba = limpieza_jugador("angelutrix")
