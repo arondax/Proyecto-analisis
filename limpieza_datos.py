5c3ff4c2f9bc7efbb553a3322df450ae1a397dd3
arondax <alexronda30@gmail.com>
2026-04-08 01:12:18 +0200

    Limpieza duplicados correcta

--- limpieza_datos.py
+++ limpieza_datos.py
@@ -14,8 +14,29 @@ def limpieza_jugador(nombre_jugador):
     
     print("Dimensiones: ", df.shape)
     print("-------------------------")
-    print("Cabecera: \n", df.columns.tolist())
+    print(df.info())
+    
+    #Borramos primero duplicados antes de borrar la columna de id de la partida
+    df= quitar_duplicados(df)
+    print("-------------------------")
+    print(df.info())
+    
+    
     
     return None
+
+"""
+The function delete the duplicate row based only in the match id.
+"""
+def quitar_duplicados(df):
+    print("Numero de duplicados: ", df.duplicated().sum())
+    #Borramos duplciados en base al id de la partida. 
+    df_limpio = df.drop_duplicates(subset=[df.columns[0]])
+    
+    return df_limpio
+
+
+
+
 
 prueba = limpieza_jugador("angelutrix")
