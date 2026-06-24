b43e04548aa353ad957015b0d62a259c6568db45
arondax <alexronda30@gmail.com>
2026-06-24 23:47:07 +0200

    Actualziar los scripts y cambiar es_main a float

--- app/schemas.py
+++ app/schemas.py
@@ -6,7 +6,7 @@ class PrediccionRequest(BaseModel):
     tag: str = Field(..., example="1234")
     region: str = Field(..., example="NA")
     mapa: str = Field(..., example="Ascent")
-    es_main: bool = Field(..., example=True)
+    es_main: float = Field(..., example=True)
     num_amigos: int = Field(..., example=5) 
     modelo: Optional[str] = Field("randomforest", description="Modelo a usar: randomforest, arboldeDecision, RegresionLineal")
 
