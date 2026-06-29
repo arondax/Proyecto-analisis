837a8b8c13c8f8caf8d36a01d40fcdbc6d69a24b
arondax <alexronda30@gmail.com>
2026-06-29 18:54:11 +0200

    Revert "Añadir estandarización a la transformación de datos numéricos"
    This reverts commit 126556b9ca1853dfb00e3819fc57c8d71d1614b9.

--- pipeline/limpieza_datos.py
+++ pipeline/limpieza_datos.py
@@ -1,8 +1,7 @@
 
 from pipeline import api, procesador
-from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder, StandardScaler
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
-
 
 import pandas as pd
 import numpy as np
@@ -196,7 +195,7 @@ def transformacion_a_numeros(df):
         - 'rango'  → Codificación ordinal respetando el orden jerárquico de rangos de Valorant
                      (Iron=0, Bronze=1, ..., Radiant=8)
         - 'mapa'   → One-Hot Encoding usando el pool de mapas ranked definido en info_valorant.json
-        - Resto    → Se hace una estandarización
+        - Resto    → Passthrough (ya son numéricas)
 
     Args:
         df (pd.DataFrame): DataFrame limpio sin columnas de texto innecesarias
@@ -237,14 +236,11 @@ def transformacion_a_numeros(df):
     ordinal_encoder = OrdinalEncoder(
         categories=[rangos]
     )
-    
-    columnas_numericas= ['kills', 'asistencias', 'muertes', 'headshots', 'acs', 'fb', 'fd', 'subrango', 'num_amigos', 'desconocidos', 'es_main']
 
     preprocessor = ColumnTransformer(
         transformers=[
             ('ord', ordinal_encoder, ordinal_features),
-            ('nom', onehot_encoder, nominal_features),
-            ('num', StandardScaler(), columnas_numericas)
+            ('nom', onehot_encoder, nominal_features)
         ],
         remainder='passthrough'
     )
@@ -253,7 +249,7 @@ def transformacion_a_numeros(df):
 
     # columnas definida antes de usarla
     columnas_nom = preprocessor.named_transformers_['nom'].get_feature_names_out(['mapa'])
-    columnas = ordinal_features + list(columnas_nom)+ columnas_numericas + [c for c in df.columns if c not in ordinal_features + nominal_features]
+    columnas = ordinal_features + list(columnas_nom) + [c for c in df.columns if c not in ordinal_features + nominal_features]
 
     df_transformado = pd.DataFrame(datos_transformados, columns=columnas)
 
